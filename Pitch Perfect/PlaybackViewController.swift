9b08a963895b5bf572a0f1e0151c0e41dc2d515c
Martin Kelly <MartinKelly@Martins-MacBook-Pro.local>
2015-08-20 14:13:46 +0100

    updated stop playback button to use new function to stop and reset all audio

--- Pitch Perfect/PlaybackViewController.swift
+++ Pitch Perfect/PlaybackViewController.swift
@@ -90,7 +90,7 @@ class PlaybackViewController: UIViewController {
     }
     
     @IBAction func stopPlayback(sender: UIButton) {
-        audioPlayer.stop()
+        stopAndResetAudio()
     }
     
     // MARK: Utility Functions
